43b6e1db432bd62ac1a45eb11164445479c24b89
Tarskan <55380090+Tarskan@users.noreply.github.com>
2022-03-14 16:23:37 +0100

    Ingredient-service

---
+++ ingredients/src/main/java/chefchallenge/backend/ingredients/Ingredient.java
@@ -0,0 +1,23 @@
+package chefchallenge.backend.ingredients;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_ingredient;
+    private String name;
+}
---
+++ ingredients/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ingredient VALUES (1, 'Steak de boeuf');
+INSERT INTO ingredient VALUES (2, 'Tomate');
+INSERT INTO ingredient VALUES (3, 'Courgette');
+INSERT INTO ingredient VALUES (4, 'Carotte');
