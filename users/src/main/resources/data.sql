3355bd551dafcf3181566b00cb197c348fcf012e
Tarskan <55380090+Tarskan@users.noreply.github.com>
2022-03-14 11:48:15 +0100

    :sparkles:User-service

---
+++ users/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user VALUES (1, 'Tristan', 'Six', 'test@hotmail.fr', 'Tarskan', 'test', NULL);
+INSERT INTO user VALUES (2, 'Thomas', 'Courrier', 'test@hotmail.fr', 'boubika', 'test', NULL);
+INSERT INTO user VALUES (3, 'Tristan', 'Six', 'test@hotmail.fr', 'Ghost', 'test', NULL);
+INSERT INTO user VALUES (4, 'Thomas', 'Handerson', 'test@hotmail.fr', 'NinjaWarrior', 'test', NULL);
