5c843548de93a0a87870223c63836aa766436567
Tarskan <55380090+Tarskan@users.noreply.github.com>
2022-03-15 13:55:12 +0100

    Admin-service-started-without-monitoring

--- challenges/src/main/java/chefchallenge/backend/challenges/ChallengesApplication.java
+++ challenges/src/main/java/chefchallenge/backend/challenges/ChallengesApplication.java
@@ -1,8 +1,10 @@
 package chefchallenge.backend.challenges;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AutoConfigurationPackage
 @SpringBootApplication
 public class ChallengesApplication {
 
--- stock/src/main/java/chefchallenge/backend/Stock/StockApplication.java
+++ stock/src/main/java/chefchallenge/backend/Stock/StockApplication.java
@@ -1,8 +1,10 @@
 package chefchallenge.backend.Stock;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AutoConfigurationPackage
 @SpringBootApplication
 public class StockApplication {
 
